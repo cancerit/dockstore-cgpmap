7677e68a6f13f26bc31d90083a2d252a806f07e5
Keiran Raine <kr2@sanger.ac.uk>
2017-01-31 09:31:11 +0000

    use TMPDIR to allow use of scripts in other situations

--- build/opt-build.sh
+++ build/opt-build.sh
@@ -2,28 +2,28 @@
 
 set -uxe
 
-rm -rf /tmp/downloads
+rm -rf $TMPDIR/downloads
 
-mkdir -p /tmp/downloads $OPT/bin $OPT/etc $OPT/lib $OPT/share $OPT/site /tmp/hts_cache
+mkdir -p $TMPDIR/downloads $OPT/bin $OPT/etc $OPT/lib $OPT/share $OPT/site /tmp/hts_cache
 
-cd /tmp/downloads
+cd $TMPDIR/downloads
 
 # cgpBigWig
 curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/cgpBigWig/archive/0.3.0.zip
-mkdir /tmp/downloads/distro
-bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
-cd /tmp/downloads/distro
+mkdir $TMPDIR/downloads/distro
+bsdtar -C $TMPDIR/downloads/distro --strip-components 1 -xf distro.zip
+cd $TMPDIR/downloads/distro
 ./setup.sh $OPT
-cd /tmp/downloads
-rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
+cd $TMPDIR/downloads
+rm -rf distro.zip $TMPDIR/downloads/distro /tmp/hts_cache
 
 # PCAP-core
 curl -sSL -o distro.zip --retry 10 https://github.com/ICGC-TCGA-PanCancer/PCAP-core/archive/v3.4.1.zip
-mkdir /tmp/downloads/distro
-bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
-cd /tmp/downloads/distro
+mkdir $TMPDIR/downloads/distro
+bsdtar -C $TMPDIR/downloads/distro --strip-components 1 -xf distro.zip
+cd $TMPDIR/downloads/distro
 ./setup.sh $OPT
-cd /tmp/downloads
-rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
+cd $TMPDIR/downloads
+rm -rf distro.zip $TMPDIR/downloads/distro /tmp/hts_cache
 
-rm -rf /tmp/downloads
+rm -rf $TMPDIR/downloads
